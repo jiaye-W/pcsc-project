6677451a59b7c4ca2215226abba42a0f66712cec
Jiaye <jiaye.wei@epfl.ch>
2023-12-08 16:10:43 +0100

    Add a new version of uniform RNG header and implementation

---
+++ random_number_generators/uniform/UniformRNG.hpp
@@ -0,0 +1,31 @@
+/*
+ * uniform-RNG.hpp
+ *
+ * Created on: Nov 30, 2023
+ *      Author: Jiaye Wei <jiaye.wei@epfl.ch>
+ */
+
+#ifndef MONTE_CARLO_UNIFORM_GENERATOR_HPP
+#define MONTE_CARLO_UNIFORM_GENERATOR_HPP
+
+#include <iostream>
+#include "../RNG.hpp"
+
+class UniformRNG : public RNG
+{
+public:
+    // Constructor and destructor
+    UniformRNG();
+    UniformRNG(int seed);
+    ~UniformRNG();
+
+    // Other public methods
+    std::vector<double> GenerateSamples(int numberOfSamples) override;
+
+private:
+    unsigned int modulus = 1e7;
+    unsigned int multiplier = 123;
+    unsigned int increment = 9999;
+};
+
+#endif //MONTE_CARLO_UNIFORM_GENERATOR_HPP
