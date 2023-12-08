ad1de9a7ebcc8431bdf22c0fa4170989b18edb8a
Jiaye <jiaye.wei@epfl.ch>
2023-12-08 16:13:01 +0100

    Add normal RNG header and implementation

---
+++ random_number_generators/normal/NormalRNG.hpp
@@ -0,0 +1,37 @@
+/*
+ * normalRNG.hpp
+ *
+ * Created on: Dec 3, 2023
+ *      Author: Jiaye Wei <jiaye.wei@epfl.ch>
+ */
+
+#ifndef MONTE_CARLO_NORMAL_RNG_HPP
+#define MONTE_CARLO_NORMAL_RNG_HPP
+
+#include <iostream>
+#include "../uniform/UniformRNG.hpp"
+#include "../RNG.hpp"
+
+class NormalRNG : public RNG
+{
+public:
+    NormalRNG();
+    NormalRNG(int seed);
+    NormalRNG(const NormalRNG& normalRng);
+    ~NormalRNG();
+
+    virtual std::vector<double> GenerateSamples(int numberOfSamples) = 0;
+
+    // Get methods
+    double GetMean() const;
+    double GetStandardDeviation() const;
+    UniformRNG GetUniformRNG() const;
+
+private:
+    double mMean;
+    double mStandardDeviation;
+    UniformRNG mUniformRNG;
+};
+
+
+#endif //MONTE_CARLO_NORMAL_RNG_HPP
