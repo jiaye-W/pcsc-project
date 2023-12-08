e48c9c13b50f2672f2169340c99dc63dd2727fed
Jiaye <jiaye.wei@epfl.ch>
2023-12-08 16:05:05 +0100

    Modify the RNG header and implementation

--- random_number_generators/RNG.hpp
+++ random_number_generators/RNG.hpp
@@ -1,8 +1,36 @@
-//
-// Created by Jiaye Wei on 01.12.2023.
-//
+/*
+ * RNG.hpp
+ *
+ * Created on: Nov 30, 2023
+ *      Author: Jiaye Wei <jiaye.wei@epfl.ch>
+ */
+
 
 #ifndef MONTE_CARLO_RNG_HPP
 #define MONTE_CARLO_RNG_HPP
+
+#include <iostream>
+
+class RNG
+{
+public:
+    // Constructor and destructor
+    RNG();
+    RNG(int seed);
+    RNG(const RNG& rng);
+    ~RNG();
+
+    // Other public methods
+    virtual std::vector<double> GenerateSamples(int numberOfSamples) = 0;
+
+    // Get methods
+    int GetSeed() const;
+
+    // Set methods
+    void SetSeed(int seed);
+
+private:
+    unsigned int mSeed;
+};
 
 #endif //MONTE_CARLO_RNG_HPP
