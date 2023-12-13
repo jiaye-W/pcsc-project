529723a03d2eeced217f37cfb5de23571c5cdba1
nguessan <mohae.nguessan@epfl.ch>
2023-12-13 19:24:24 +0100

    Add Boost submodule

---
+++ cmake-build-debug/external/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/EPFL/GitHub/pcsc_project/external/googletest/googletest
+# Build directory: D:/EPFL/GitHub/pcsc_project/cmake-build-debug/external/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
