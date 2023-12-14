b096f76ffa34e3e5ac080e92d42699be38cc73cf
nguessan <mohae.nguessan@epfl.ch>
2023-12-14 10:22:54 +0100

    decided to only gitsubmodule boost-math and relevent subpart

--- cmake-build-debug/external/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/EPFL/GitHub/pcsc_project/external/googletest/googlemock
-# Build directory: D:/EPFL/GitHub/pcsc_project/cmake-build-debug/external/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-debug/external/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/EPFL/GitHub/pcsc_project/external/googletest/googletest
-# Build directory: D:/EPFL/GitHub/pcsc_project/cmake-build-debug/external/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
