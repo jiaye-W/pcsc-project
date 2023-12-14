9e7ffbe4652f39f79c14eb19bbef18f86ec81c28
Jiaye <jiaye.wei@epfl.ch>
2023-12-14 16:02:32 +0100

    Remove submodule external/boost

--- cmake-build-debug/external/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jiayewei927/Desktop/pcsc-project/external/googletest
-# Build directory: /Users/jiayewei927/Desktop/pcsc-project/cmake-build-debug/external/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jiayewei927/Desktop/pcsc-project/googletest/googletest
-# Build directory: /Users/jiayewei927/Desktop/pcsc-project/cmake-build-debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
